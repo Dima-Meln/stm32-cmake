a4e617c5a88b851c86f3c00a70f0e21daf110322
Dima-Meln <dimamelnichuk11@gmail.com>
2012-03-24 09:03:21 +0200

    Signed-off-by: Dima-Meln <dimamelnichuk11@gmail.com>

---
+++ stm32f4/cmsis/build/CMakeFiles/stm32f4_cmsis.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/dima/projects/git/stm32-cmake/stm32f4/libs/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c" "/home/dima/projects/git/stm32-cmake/stm32f4/cmsis/build/CMakeFiles/stm32f4_cmsis.dir/home/dima/projects/git/stm32-cmake/stm32f4/libs/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "STM32F4xx"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
